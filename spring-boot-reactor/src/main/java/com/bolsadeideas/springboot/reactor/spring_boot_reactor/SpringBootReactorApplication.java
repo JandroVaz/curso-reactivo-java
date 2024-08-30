df6650ac9751849120d0847b12ec60fc1aed28aa
Alejandro Manuel <jandrovaz99@gmail.com>
2024-08-30 12:00:55 +0200

    [Add] Curso de Java reactivo

---
+++ spring-boot-reactor/src/main/java/com/bolsadeideas/springboot/reactor/spring_boot_reactor/SpringBootReactorApplication.java
@@ -0,0 +1,47 @@
+package com.bolsadeideas.springboot.reactor.spring_boot_reactor;
+
+import com.bolsadeideas.springboot.reactor.spring_boot_reactor.models.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<Usuario> nombres = Flux.just("Andres sanchez", "Pedro Cortes", "Diego San", "Juan Carrasco", "Paco Perico", "Bruce Lee", "Bruce Willy")
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> usuario.getNombre().toLowerCase().equals("bruce"))
+				.doOnNext(usuario -> {
+					if(usuario == null){
+						throw new RuntimeException("Nombres no puede ser vacíos");
+					}
+					System.out.println(usuario);
+				}).map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(usuario.getNombre().concat(" ".concat(usuario.getApellido())));
+					return usuario;
+				});
+
+
+		// Subscribe
+		nombres.subscribe(e -> log.info(e.getNombre()),
+				error -> log.error(error.getMessage()),
+				//Multitarea que se ejecuta en paralelo
+				new Runnable() {
+					@Override
+					public void run() {
+						log.info("Ha finalizado la ejecución del observable con éxito");
+					}
+				});
+	}
+}
---
+++ spring-boot-reactor/src/test/java/com/bolsadeideas/springboot/reactor/spring_boot_reactor/SpringBootReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.reactor.spring_boot_reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
